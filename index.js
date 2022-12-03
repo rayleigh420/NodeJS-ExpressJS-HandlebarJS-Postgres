cd18f6a48506653ed9690000bee59de507cc696e
Yugiobattu420 <nhatduy0409@gmail.com>
2022-12-03 10:19:34 +0700

    Add automaticaaly refresh browser if hbs file change

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 import express from 'express'
 require('dotenv').config()
 // const expressHbs = require("express-handlebars")
+import path from 'path'
 import { engine } from 'express-handlebars';
+import livereload from 'livereload';
+import connectLiveReload from 'connect-livereload'
 
 import initTestRoute from './routes/testRoute';
 
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, 'views'));
+liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+        liveReloadServer.refresh("/");
+    }, 100);
+});
+
 const app = express()
 const port = process.env.PORT || 8080
+app.use(connectLiveReload());
 
 app.engine('hbs', engine({
     layoutsDir: __dirname + "/views/layouts",
